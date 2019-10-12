33152841ba4c7d37a46f9623bf3733e9bcbefe9c
netarrow <netarrow89@hotmail.com>
2019-10-12 15:35:03 +0200

    port configurable

--- project/server.js
+++ project/server.js
@@ -90,7 +90,7 @@ app.post('/api/delete', (req, res) => {
   });
 });
 
-const server = app.listen(80, () => {
+const server = app.listen(process.env.PORT || 1337, () => {
 
   const host = server.address().address;
   const port = server.address().port;
